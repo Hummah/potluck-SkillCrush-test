cc897f16393855b023adc092a0849e1c7fa6f8a5
Huma <humanawaz655@gmail.com>
2022-09-26 09:23:07 +0500

    done writting guest listing javaScript

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-// // invite button
-// const addGuestButton = document.querySelector(".invite");
-// // label for the invite button
-// const guestInputLabel = document.querySelector(".add-guest label");
-// // text input box
-// const guestInput = document.querySelector(".add-guest input");
-// // unordered list (not yet visible)
-// const guestList = document.querySelector(".guest-list");
-// // span class for number of guests attending
-// const guestCount = document.querySelector(".attendance");
-// // alert when guest list is full (not yet visible)
-// const guestFull = document.querySelector(".alert");
-// // to assign dishes button
-// const assignButton=document.querySelector(".assign");
-// // name of dishes 
-// const assignItems = document.querySelector(".assigned-items");
-// // show footer
-// const footer= document.querySelector("footer");
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+// to assign dishes button
+const assignButton=document.querySelector(".assign");
+// name of dishes 
+const assignItems = document.querySelector(".assigned-items");
+// show footer
+const footer= document.querySelector("footer");
 
-// const refresh=document.querySelector(".refresh");
+const refresh=document.querySelector(".refresh");
 
 
-// addGuestButton.onclick=()=>{
-// let guest=guestInput.value;
-// // console.log(guest);
-// condition(guest);
-// guestInput.value="";
-// };
-// let condition = function(guest){
-//     if(guest !== ""){
-// let listItem = document.createElement("li");
-// listItem.innerText=guest;
-// guestList.append(listItem);
-//     }
-// guestMax();
-// };
-// let guestMax= function(){
-//     let invited= document.querySelectorAll(".guest-list li");
-//     guestCount.innerText=invited.length;
-//     if(invited.length === 8){
-//         addGuestButton.classList.add("hide");
-//         guestInput.classList.add("hide");
-//         guestFull.classList.remove("hide");
-//         guestInputLabel.classList.add("hide");
-//     }
-// };
+addGuestButton.onclick=()=>{
+let guest=guestInput.value;
+// console.log(guest);
+condition(guest);
+guestInput.value="";
+};
+let condition = function(guest){
+    if(guest !== ""){
+let listItem = document.createElement("li");
+listItem.innerText=guest;
+guestList.append(listItem);
+    }
+guestMax();
+};
+let guestMax= function(){
+    let invited= document.querySelectorAll(".guest-list li");
+    guestCount.innerText=invited.length;
+    if(invited.length === 8){
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestFull.classList.remove("hide");
+        guestInputLabel.classList.add("hide");
+    }
+};
 
 // let dishes= function(){
 //     let potlukItems = ["potato salad","rass malai","biryani","juice","white chicken","beef pulao","pizza","white sauce pasta","chicken roast","lazaniya"];
